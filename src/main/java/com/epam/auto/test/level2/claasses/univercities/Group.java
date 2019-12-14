3d8fa73722c55d94b7d6018db6d507ca0c8ff029
Petr <stirlitz03@rambler.ru>
2019-12-14 20:45:11 +0300

    Done task 2.1

---
+++ src/main/java/com/epam/auto/test/level2/claasses/univercities/Group.java
@@ -0,0 +1,53 @@
+package com.epam.auto.test.level2.claasses.univercities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.epam.auto.test.level2.claasses.univercities.Student;
+import com.epam.auto.test.level2.exceptions.InvalidDataException;
+
+public class Group {
+	private String name;
+	private List<Student> students = new ArrayList<>();
+	
+	public Group(String name, List<Student> students) throws InvalidDataException {
+		this.name = name;
+		if (students != null && students.size() != 0) {
+			this.students = students;
+		} else {
+			throw new InvalidDataException(name + " group has no students: ");
+		}
+	}
+	
+	public Group() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void setStudents(List<Student> students) throws InvalidDataException {
+		if (students != null && students.size() != 0) {
+			this.students = students;
+		} else {
+			throw new InvalidDataException(name + " group has no students: ");
+		}
+	}
+
+	public void print() {
+		System.out.println("  " + name + ":");
+		for (Student student : students) {
+			student.print();
+		}
+	}
+	
+}
